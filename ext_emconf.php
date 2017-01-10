c64e198fd460974cc85714e08c27a257f74a6765
Joe Schäfer <schaefer@scw.info>
2017-01-10 16:03:56 +0100

    initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,24 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'ratios for imageManipulation',
+    'description' => 'Ratios for image manipulation in TYPO3 CMS Backend ',
+    'category' => 'misc',
+    'version' => '1.0.0',
+    'state' => 'stable',
+    'uploadfolder' => false,
+    'createDirs' => '',
+    'clearcacheonload' => false,
+    'author' => 'Joe Schäfer',
+    'author_email' => 'joe@joekola.de',
+    'author_company' => '',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '7.6.0-7.9.99',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+);
