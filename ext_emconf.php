f223d875ecdedbf1c0611a796d4fdbda950a000a
Joe Schäfer <schaefer@scw.info>
2017-01-10 16:28:26 +0100

    change requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
     'author_company' => '',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '7.6.0-7.9.99',
+            'typo3/cms' => '7.6.0-8.9.99',
         ),
         'conflicts' => array(
         ),
